8684b16c8df9ccd6aa00b4ac0a92aac6ce992e9c
janhamedi49@gmail.com <janhamedi49@gmail.com>
2021-08-06 06:56:55 -0500

    initial

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php 
 require_once __DIR__  . '/../vendor/autoload.php';
+use app\core\App;
 use app\controller\Home;
 use app\controller\Student;
-use app\core\App;
 $app = new App();
 $home = new Home();
 $student = new Student();
